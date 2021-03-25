a72886cae617090653f0bc69887d557e566735b0
cebarbosa <kadu.barbosa@gmail.com>
2021-03-25 18:14:59 -0300

    Backing up incomplete codes.

--- prepare_data.py
+++ prepare_data.py
@@ -15,10 +15,13 @@ import context
 
 if __name__ == "__main__":
     sample = "test"
-    data_dir = os.path.join(context.data_dir, "test")
+    data_dir = os.path.join(context.data_dir, sample)
     galaxies = os.listdir(data_dir)
     wranges = ["UVB", "VIS", "NIR"]
     R = [3300, 5400, 3500]
+    target_res = 100.
+    velscale = 50.
+    c = const.c.to("km/s").value
     for galaxy in galaxies:
         wdir = os.path.join(data_dir, galaxy)
         for i, wr in enumerate(wranges):
@@ -26,9 +29,10 @@ if __name__ == "__main__":
             errorspecname = specname.replace("FLUX", "ERROR")
             spec1d = Spectrum1D.read(os.path.join(wdir, specname))
             wave = np.power(10, spec1d.spectral_axis.value) * u.angstrom
-            flux = spec1d.flux
+            target_fwhm = target_res / c * wave * 2.355
+            # flux = spec1d.flux
             fwhm = wave/ R[i]
-            vel = const.c.to("km/s") * fwhm / wave / 2.634
-            print(vel)
-            plt.plot(wave,  vel)
+            # sigma = np.full(len(wave), const.c.to("km/s").value / 2.355 / R[i])
+            plt.plot(wave,  target_fwhm, c="C{}".format(i))
+            plt.plot(wave, fwhm, c="C{}".format(i), ls="--")
         plt.show()
---
+++ prepare_fsps.py
@@ -0,0 +1,148 @@
+"""
+Prepare templates for use with paintbox
+"""
+import os
+import itertools
+
+import numpy as np
+import astropy.units as u
+import astropy.constants as const
+from astropy.table import Table, hstack, vstack
+from astropy.io import fits
+import ppxf.ppxf_util as util
+from tqdm import tqdm
+from scipy.interpolate import interp1d
+import matplotlib.pyplot as plt
+
+from spectres import spectres
+
+import context
+import fsps
+
+def make_FSPS_varydoublex(outdir, redo=False):
+    """ Use python-FSPS to produce models to be used with paintbox. """
+    if not os.path.exists(outdir):
+        os.mkdir(outdir)
+    flam_unit = u.erg / u.cm / u.cm / u.s / u.AA
+    fnu_unit = u.erg / u.s / u.cm / u.cm / u.Hz
+    bands = fsps.list_filters()
+    sp = fsps.StellarPopulation(compute_vega_mags=False, zcontinuous=1,
+                                sfh=0, logzsol=0.0, dust_type=2, dust2=0.,
+                                add_agb_dust_model=False,
+                                add_dust_emission=False,
+                                add_neb_emission=False,
+                                nebemlineinspec=False,
+                                imf_type=2, smooth_lsf=1, sigma_smooth=100)
+    wave_eff, Mvega, Msun = sp.filter_data()
+    Zs = np.array([-2.5, -2.0, -1.75, -1.5, -1.25, -1., -0.75, -0.5, -0.25,
+                   0., 0.25, 0.5])
+    logTs = np.arange(5, 10.31, 0.05)
+    Ts = np.power(10, logTs) / 1e9
+    imf1s = 0.5 + np.arange(16) / 5
+    imf2s = 0.5 + np.arange(16) / 5
+    params = list(itertools.product(*[Zs, imf1s, imf2s]))
+    for Z, imf1, imf2 in tqdm(params, desc="Processing spectra with FSPS"):
+        zname = "{:+.2f}".format(Z).replace("+", "p").replace("-", "m")
+        x1name = "{:+.2f}".format(imf1).replace("+", "p").replace("-", "m")
+        x2name = "{:+.2f}".format(imf2).replace("+", "p").replace("-", "m")
+        fname = "FSPS_varydoublex_Z{}_imf1{}_imf2{}.fits".format(zname, x1name,
+                                                           x2name)
+
+        output = os.path.join(outdir, fname)
+        if os.path.exists(output) and not redo:
+            continue
+        sp.params['logzsol'] = Z
+        sp.params["imf1"] = imf1
+        sp.params["imf2"] = imf2
+        wave, fnu = sp.get_spectrum()
+        # Making table with magnitudes of models
+        mags = sp.get_mags(bands=bands)
+        tabmags = Table(mags, names=bands)
+        # Calculating M/L
+        Mstar = sp.stellar_mass
+        mstar_table = Table([Mstar], names=["M*+rem"])
+        L = np.power(10, -0.4 * (mags - Msun[None, :]))
+        M2L = Mstar[:,None] / L
+        names_M2L = ["ML_{}".format(b) for b in bands]
+        tabM2L = Table(M2L, names=names_M2L)
+        # Processing spectra
+        fnu = fnu * const.L_sun / u.Hz / (4 * np.pi * const.au**2)
+        fnu = fnu.to(fnu_unit)
+        flam = fnu * const.c / (wave * u.AA)**2
+        flam = flam.to(flam_unit).value
+        ones = np.ones(len(Ts))
+        tab = Table([Z * ones, Ts, imf1 * ones, imf2 * ones],
+                       names=["logzsol", "age", "imf1", "imf2"])
+        otable = hstack([tab, mstar_table, tabmags, tabM2L])
+        # Saving results in output
+        hdu1 = fits.PrimaryHDU(flam)
+        hdu1.header["EXTNAME"] = "SSPS"
+        hdu1.header["BUNIT"] = ("{}".format(flam_unit),
+                               "Physical units of the array values")
+        hdu2 = fits.BinTableHDU(otable)
+        hdu2.header["EXTNAME"] = "PARAMS"
+        # Making wavelength array
+        hdu3 = fits.BinTableHDU(Table([wave], names=["wave"]))
+        hdu3.header["EXTNAME"] = "WAVE"
+        hdulist = fits.HDUList([hdu1, hdu2, hdu3])
+        hdulist.writeto(output, overwrite=True)
+
+def prepare_CvD_templates(wave, output, redo=False):
+    """ Use FSPS models to prepare templates for a given instrumental setup. """
+    if os.path.exists(output) and not redo:
+        return
+    global FSPS_dir
+    filenames = os.listdir(FSPS_dir)
+    Zs = np.sort(list(set(
+                 [float(_.split("_")[2].replace(
+                  "Zm", "-").replace( "Zp", "+")) for _ in filenames])))
+    imfs = np.sort(list(set([float(_.split("_")[3].replace(
+                   "imf1p", "+")) for _ in filenames])))
+    pars = np.array(np.meshgrid(Zs, imfs, imfs, indexing='ij')).reshape(3, -1).T
+    specs, outtab = [], []
+    for j, (Z, imf1, imf2) in enumerate(tqdm(pars)):
+        zname = "{:+.2f}".format(Z).replace("+", "p").replace("-", "m")
+        x1name = "{:+.2f}".format(imf1).replace("+", "p").replace("-", "m")
+        x2name = "{:+.2f}".format(imf2).replace("+", "p").replace("-", "m")
+        fname = "FSPS_varydoublex_Z{}_imf1{}_imf2{}.fits".format(zname, x1name,
+                                                           x2name)
+        data = fits.getdata(os.path.join(FSPS_dir, fname))
+        wmodel = Table.read(os.path.join(FSPS_dir, fname), hdu=2)["wave"].data
+        outspecs = np.zeros((len(data), len(wave)))
+        for i, spec in enumerate(data):
+            outspecs[i] = interp1d(wmodel, spec)(wave)
+        table = Table.read(os.path.join(FSPS_dir, fname), hdu=1)
+        if j == 0:
+            specs = outspecs
+            outtab = table
+        else:
+            specs = np.vstack([specs, outspecs])
+            outtab = vstack([outtab, table])
+    flam_unit = u.erg / u.cm / u.cm / u.s / u.AA
+    # Saving results in output
+    hdu1 = fits.PrimaryHDU(specs)
+    hdu1.header["EXTNAME"] = "SSPS"
+    hdu1.header["BUNIT"] = ("{}".format(flam_unit),
+                            "Physical units of the array values")
+    hdu2 = fits.BinTableHDU(outtab)
+    hdu2.header["EXTNAME"] = "PARAMS"
+    # Making wavelength array
+    hdu3 = fits.BinTableHDU(Table([wave], names=["wave"]))
+    hdu3.header["EXTNAME"] = "WAVE"
+    hdulist = fits.HDUList([hdu1, hdu2, hdu3])
+    hdulist.writeto(output, overwrite=True)
+
+def prepare_models_wifis(w1=8600, w2=13200, velscale=200):
+    logLam, velscale = util.log_rebin([w1, w2], np.ones(10000),
+                                               velscale=velscale)[1:]
+    wave = np.exp(logLam) * u.AA
+    output = "/home/kadu/Dropbox/SPINS/fsps_wifis.fits"
+    prepare_CvD_templates(wave, output, redo=False)
+
+if __name__ == "__main__":
+    FSPS_dir = "/home/kadu/Dropbox/FSPS/varydoublex"
+    run_FSPS = False
+    if run_FSPS:
+        outdir = FSPS_dir
+        make_FSPS_varydoublex(outdir, redo=True)
+    prepare_models_wifis()
